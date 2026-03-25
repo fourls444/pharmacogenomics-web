47b34694f7c59c41b3fd6cc38271469869a03c21
Thanon <fourdat.444@gmail.com>
2026-03-25 14:35:18 +0700

    add registration

--- app/royal-college/page.tsx
+++ app/royal-college/page.tsx
@@ -4,10 +4,12 @@ import HistorySection from "./components/HistorySection/HistorySection";
 import CollegesSection from "./components/CollegesSection/CollegesSection";
 import MeetingsSection from "./components/MeetingsSection/MeetingsSection";
 import NewsSection from "./components/NewsSection/NewsSection";
+import RegistrationModal from "../components/ui/RegistrationModal";
 
 export default function RoyalCollegePage() {
     return (
         <div style={{ fontFamily: "var(--font-ibm-plex-sans-thai), sans-serif" }}>
+            <RegistrationModal />
             <HeroSection />
             <AboutSection />
             <HistorySection />
